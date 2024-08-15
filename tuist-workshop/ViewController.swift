453c42743ec358e36d84146492624d9842a762d3
shlykovdanylo <shlykovdanylo@gmail.com>
2024-08-15 14:03:07 +0200

    Initial Commit

---
+++ tuist-workshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tuist-workshop
+//
+//  Created by Danylo on 15/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
